b03522f88d5a63f28858988dfbd31f0f6ab98dd4
hunit <hunit612@gmail.com>
2021-02-12 17:38:34 +0900

    create forms.py

--- community/user/views.py
+++ community/user/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import User
+from .forms import LoginForm
 
 
 def home(request):
@@ -22,27 +23,16 @@ def logout(request):
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # session
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
 
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            cmuser = User.objects.get(username=username)
-            if check_password(password, cmuser.password):
-                # 비밀번호가 일치, 로그인 처리
-                # SESSION
-                request.session['user'] = cmuser.id
-                # redirect
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호 틀렸습니다.'
-
-        return render(request, 'login.html', res_data)
+    return render(request, 'login.html', {'form':form})
 
 
 def register(request):
