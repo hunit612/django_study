0f6e2b321b4a9534f4eb758dd642c6a22b579ae1
hunit <hunit612@gmail.com>
2021-02-13 16:01:28 +0900

    update home page and add button

--- community/user/views.py
+++ community/user/views.py
@@ -6,13 +6,7 @@ from .forms import LoginForm
 
 
 def home(request):
-    user_id = request.session.get('user')
-
-    if user_id:
-        cmuser = User.objects.get(pk=user_id)
-        return HttpResponse(cmuser.username)
-
-    return HttpResponse('Home!')
+    return render(request, 'home.html')
 
 
 def logout(request):
